81308f0ca14d63879b47059889ad98791cd1da7d
郭奕 <guoyi@163.com>
2019-07-21 01:36:13 +0800

    学习了作用域插槽

--- slot/src/App.vue
+++ slot/src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld>
+      qqqqqerwerwerwer
+      <div slot="left">我是具名插槽left</div>
+      <div slot="right">我是具名插槽1</div>qqqqqerwerwerwer
+    </HelloWorld>-->
+    <!--
+        #default  ===  v-slot:default
+        {user:myName}  ===  将user属性重命名为myName
+
+     -->
+    <ScopeSlot :user="user">
+      <template #default="{ parentProp = { firstName: 'Guest' } }">
+        {{ parentProp.firstName }}
+      </template>
+    </ScopeSlot>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import ScopeSlot from './components/ScopeSlot.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    data() {
+      return {
+        user: {
+          firstName: "郭",
+          lastName: "奕",
+        }
+      }
+    },
+    components: {
+      HelloWorld, ScopeSlot
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ slot/src/components/ScopeSlot.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <span>
+      <slot :user="user">{{ user.lastName }}</slot>
+      {{ user.lastName }}
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ScopeSlot",
+    props:{
+      user:{
+        type:Object
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
